727b09c2e382458466024466a9ae3a43fda8a41d
Alfonso Contreras <pon.alfonsoirai@gmail.com>
2017-10-04 20:45:34 -0500

    commit

--- phil.c
+++ phil.c
@@ -7,8 +7,10 @@
 
 #define NUM_PHIL     5
 #define NUM_FORK 5
+#define NUM_CHAIRS NUM_FORK/2
 
 sem_t forks[NUM_FORK];
+sem_t chairs;
 int getRand(float max){
     return 1 + (rand() * max) / RAND_MAX;
 }
@@ -27,17 +29,14 @@ void *philLive(void *threadid){
     while(1){
         printf("I'm Phil %ld , thinking\n", tid);
         thinking();
-        if(tid % 2 == 0){
-            sem_wait(&forks[tid]);
-            sem_wait(&forks[(tid + 1) % NUM_PHIL]);
-        } else{
-            sem_wait(&forks[(tid + 1) % NUM_PHIL]);
-            sem_wait(&forks[tid]);
-        }
+        sem_wait(&chairs);
+        sem_wait(&forks[tid]);
+        sem_wait(&forks[(tid + 1) % NUM_PHIL]);
         printf("I'm Phil %ld , eating\n", tid);
         eating();
         sem_post(&forks[tid]);
         sem_post(&forks[(tid + 1) % NUM_PHIL]);
+        sem_post(&chairs);
     }
 
     pthread_exit(NULL);
@@ -52,7 +51,7 @@ int main (int argc, char *argv[])
    for(t=0; t<NUM_PHIL; t++){
        sem_init(&forks[t],0,1);
    }
-
+   sem_init(&chairs, 0,     NUM_CHAIRS);
    for(t=0; t<NUM_PHIL; t++){
       rc = pthread_create(&philosophers[t], NULL, philLive, (void *)t);
       if (rc){
